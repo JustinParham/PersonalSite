bda801368be6ea7869f1716ec980e7b3a2b7558d
unknown <justinjparham@gmail.com>
2021-04-27 20:24:42 -0700

    corrected spelling and grammar mistakes

--- components/intro/Intro.js
+++ components/intro/Intro.js
@@ -10,12 +10,11 @@ export default function IntroArea() {
           I am a new web dev with a passion for learning and problem solving. I
           utlilized my time stuck indoors during Covid Quarantine to learn web
           development skills in an attempt to make a career change. I am
-          searching for a employment in the fields of front-end or full stack
-          web development.
+          searching for employment in front-end or full stack web development.
         </p>
         <p>
           I am a former College and High School Track and Field Coach who excels
-          in communication and technical explanations. I can
+          in communication and technical explanations.
         </p>
       </div>
     </div>

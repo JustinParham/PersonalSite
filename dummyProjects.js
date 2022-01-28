e4b00b61d0585944bb76bea48302c61dfbe32ca8
unknown <justinjparham@gmail.com>
2022-01-28 14:47:46 -0800

    added faux-store project to project page

--- dummyProjects.js
+++ dummyProjects.js
@@ -1,19 +1,26 @@
 const dummyProjects = [
   {
-    title: "Pokemon Data Site",
-    description:
-      "Small site that uses data pulled from PokeAPI to lookup pokemon by number, as well as contains a pokemon matching game",
-    imageURL: "/images/projectImages/pokeAPI.jpg",
-    id: "2",
-    projectURL: "https://poke-site.herokuapp.com/",
+    title: 'Faux-Store',
+    description: 'E-store example project',
+    imageURL: '/images/projectImages/faux-store.jpg',
+    id: '3',
+    projectURL: 'https://faux-store.herokuapp.com',
   },
   {
-    title: "CA Tide Data",
+    title: 'Pokemon Data Site',
     description:
-      "Simple project to retrieve data about California Coastal tides from NoAA API",
-    imageURL: "/images/caTides.jpg",
-    id: "1",
-    projectURL: "http://Catidedata.herokuapp.com",
+      'Small site that uses data pulled from PokeAPI to lookup pokemon by number, as well as contains a pokemon matching game',
+    imageURL: '/images/projectImages/pokeAPI.jpg',
+    id: '2',
+    projectURL: 'https://poke-site.herokuapp.com/',
+  },
+  {
+    title: 'CA Tide Data',
+    description:
+      'Simple project to retrieve data about California Coastal tides from NoAA API',
+    imageURL: '/images/caTides.jpg',
+    id: '1',
+    projectURL: 'http://Catidedata.herokuapp.com',
   },
 ];
 
